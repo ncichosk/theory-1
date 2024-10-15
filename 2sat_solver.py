74956f613713729e9362a410f8bcf24a1b74ac61
ncichosk <157041507+ncichosk@users.noreply.github.com>
2024-10-14 20:31:23 -0400

    graph edition

--- 2sat_solver.py
+++ 2sat_solver.py
@@ -1,47 +1,63 @@
 #!/usr/bin/env python3
 
+# need to install matplotlib
+
 # Theory of Computing 2SAT solver
 
 import time
-import random
-import string
 import sys
 import csv
+import matplotlib.pyplot as plt
+
+def plot(x,y, name):
+    plt.plot(x, y)
+
+    plt.title("Time Complexity for {name}")
+
+    plt.xlabel("Variables in SAT Problem")
+    plt.ylabel("Time to Compute (microseconds)")
+
+    plt.show()
+
+def plot_both(x,y):
+    pass
 
 def DPLL_check(Wff, solution):
     for pair in Wff:
-        if (solution[pair[0]] == 0 and solution[pair[1]] == 0):
+        a = pair[0]
+        b = pair[1]
+        
+        if a > 0:
+            a_val = solution[a-1]
+        else:
+            a_val = 1 - solution[abs(a)-1] if solution[abs(a)-1] != -1 else -1
+        
+        if b > 0:
+            b_val = solution[b-1]
+        else:
+            b_val = 1 - solution[abs(b)-1] if solution[abs(b)-1] != -1 else -1
+        
+        if a_val == 0 and b_val == 0:
             return False
     return True
 
 
 def DPLL (Wff, Nvars, Nclauses, solution, Pos):
-    found = False
     if Pos == Nvars:
-        return False
-    solution[Pos] = 0
-    solution[-Pos] = 1
-    val = DPLL_check(Wff, solution)
-    if (val and (Nvars == Pos - 1)):
-        return True
-    elif (val):
-        found = DPLL(Wff, Nvars, Nclauses, solution, Pos)
-        if found == True:
-            return True
-    else:
-        return False
+        return DPLL_check(Wff, solution)
     
-    solution[Pos] = 1
-    solution[-Pos] = 0
-    val = DPLL_check(Wff, solution)
-    if (val and (Nvars == Pos - 1)):
-        return True
-    elif (val):
-        found = DPLL(Wff, Nvars, Nclauses, solution, Pos)
-        if found == True:
+    solution[Pos] = 0
+    if DPLL_check(Wff, solution):
+        if DPLL(Wff, Nvars, Nclauses, solution, Pos + 1):
             return True
-    else:
-        return False
+
+    solution[Pos] = 1
+    if DPLL_check(Wff, solution):
+        if DPLL(Wff, Nvars, Nclauses, solution, Pos + 1):
+            return True
+    
+    solution[Pos] = -1
+    return False
 
 def check(Wff,Nvars,Nclauses,Assignment):
 # Run thru all possibilities for assignments to wff
@@ -73,11 +89,17 @@ def check(Wff,Nvars,Nclauses,Assignment):
     return Satisfiable
 
 def test_wff(wff,Nvars,Nclauses):
-    #Assignment=list((0 for x in range(Nvars+2))) # Original Check
-    Assignment = list((-1 for x in range(Nvars*2))) # DPLL Check
+    Assignment=list((0 for x in range(Nvars+2))) # Original Check
     start = time.time() # Start timer
-    #SatFlag=check(wff,Nvars,Nclauses,Assignment)
-    SatFlag=DPLL(wff, Nvars, Nclauses, Assignment)
+    SatFlag=check(wff,Nvars,Nclauses,Assignment)
+    end = time.time() # End timer
+    exec_time=int((end-start)*1e6)
+    return [wff,Assignment,SatFlag,exec_time]
+
+def test_wff_DPLL(wff,Nvars,Nclauses):
+    Assignment = list((-1 for x in range(Nvars))) # DPLL Check
+    start = time.time() # Start timer
+    SatFlag=DPLL(wff, Nvars, Nclauses, Assignment, 0)
     end = time.time() # End timer
     exec_time=int((end-start)*1e6)
     return [wff,Assignment,SatFlag,exec_time]
@@ -86,6 +108,7 @@ def main():
     wffs = {}  
     wff_num = 1
     current_wff = "wff_1"
+    mode = int(sys.argv[2])
 
     try:
         with open(sys.argv[1], 'r') as file:
@@ -100,17 +123,75 @@ def main():
         print(f'Error: File {sys.argv[1]} invalid \n')
         return -1
     
+    reg_time = 0
+    dp_time = 0
+    vars = []
+    time_reg = []
+    time_dp = []
+
     for key, wff in wffs.items():
         nvars = int(wff[1][2])
         nclauses = int(wff[1][3])
-        assignment = wff[0][3]
+
         data = wff[2:]
         data = [row[:2] for row in data]
         data = [list(map(int, row)) for row in data]
 
-        output = test_wff(data, nvars, nclauses)
-        print(output)
-        return 0
+        match mode:
+            case 0:
+                output = test_wff(data, nvars, nclauses)
+                print(f"Is {key} satisfiable? {'Yes' if output[2] else 'No'}")
+                if output[2]:
+                    print(f'Solution: {output[1][1:-1]}')
+                else:
+                    print(f'Solution: None')
+                print(f'Time: {output[3]}')
+
+                reg_time += output[3]
+                vars.append(nvars)
+                time_reg.append(output[3])
+
+            case 1:
+                output_DPLL = test_wff_DPLL(data, nvars, nclauses)
+                print(f"Is {key} satisfiable? {'Yes' if output_DPLL[2] else 'No'}")
+                if output_DPLL[2]:
+                    print(f'Solution: {output_DPLL[1]}')
+                else:
+                    print(f'Solution: None')
+                print(f'Time: {output_DPLL[3]}')
+
+                dp_time += output_DPLL[3]
+                vars.append(nvars)
+                time_dp.append(output_DPLL[3])
+
+            case 2:
+                issues = []
+                output = test_wff(data, nvars, nclauses)
+                output_DPLL = test_wff_DPLL(data, nvars, nclauses)
+                reg_time += output[3]
+                dp_time += output_DPLL[3]
+                if (output[2] != output_DPLL[2]):
+                    issues.append((output, output_DPLL))
+                print(f'{key} of {len(wffs)}:\n')
+                print(f'DumbSAT Output: {output[2]} \t DPLL Output: {output_DPLL[2]}\n')
+                print(f'DumbSAT Time: {output[3]} \t DPLL Time: {output_DPLL[3]}\n')
+
+                vars.append(nvars)
+                time_dp.append(output_DPLL[3])
+                time_reg.append(output[3])
+
+    match mode:
+        case 0:
+            print(f'DumbSAT Total Time: {reg_time}')
+            plot(vars, time_reg, "DumbSAT")
+        case 1:
+            print(f'DPLL Total Time: {dp_time}')
+            plot(vars, time_dp, "DPLL")
+        case 2:
+            print(f'DumbSAT Total Timae: {reg_time} \t DPLL Total Time: {dp_time}\n')
+            print(f'Conflicting Outputs: {issues}')
+
+    return 0
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
