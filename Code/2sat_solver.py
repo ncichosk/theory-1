127ddce13c6b7c282c1ad941e6d4d62054a7b80b
ncichosk <157041507+ncichosk@users.noreply.github.com>
2024-10-16 23:35:27 -0400

    done for now

--- Code/2sat_solver.py
+++ Code/2sat_solver.py
@@ -51,77 +51,85 @@ def main():
     time_dp = []
     truths = []
 
-    for key, wff in wffs.items():
-        nvars = int(wff[1][2])
-        nclauses = int(wff[1][3])
-
-        data = wff[2:]
-        data = [row[:2] for row in data]
-        data = [list(map(int, row)) for row in data]
-
-
-        if mode  == 0:
-            output = test_wff(data, nvars, nclauses)
-            print(f"Is {key} satisfiable? {'Yes' if output[2] else 'No'}")
-            if output[2]:
-                print(f'Solution: {output[1][1:-1]}')
-            else:
-                print(f'Solution: None')
-            print(f'Time: {output[3]}')
-
-            reg_time += output[3]
-            vars.append(nvars)
-            time_reg.append(output[3])
-            truths.append(output[2])
-
-        elif mode == 1:
-            output_DPLL = test_wff_DPLL(data, nvars, nclauses)
-            print(f"Is {key} satisfiable? {'Yes' if output_DPLL[2] else 'No'}")
-            if output_DPLL[2]:
-                print(f'Solution: {output_DPLL[1]}')
-            else:
-                print(f'Solution: None')
-            print(f'Time: {output_DPLL[3]}')
-
-            dp_time += output_DPLL[3]
-            vars.append(nvars)
-            time_dp.append(output_DPLL[3])
-            truths.append(output_DPLL[2])
-
-        elif mode == 2:
-            issues = []
-            output = test_wff(data, nvars, nclauses)
-            output_DPLL = test_wff_DPLL(data, nvars, nclauses)
-            reg_time += output[3]
-            dp_time += output_DPLL[3]
-            if (output[2] != output_DPLL[2]):
-                issues.append((output, output_DPLL))
-            print(f'{key} of {len(wffs)}:\n')
-            print(f'DumbSAT Output: {output[2]} \t DPLL Output: {output_DPLL[2]}\n')
-            print(f'DumbSAT Time: {output[3]} \t DPLL Time: {output_DPLL[3]}\n')
-
-            vars.append(nvars)
-            time_dp.append(output_DPLL[3])
-            time_reg.append(output[3])
-
-
     if mode == 0:
-        print(f'DumbSAT Total Time: {reg_time}')
-        name = "DumbSAT"
-        plot(vars, time_reg, name, truths)
+        outfile_path = '../Output/dumbSAT_output.txt'
     elif mode == 1:
-        print(f'DPLL Total Time: {dp_time}')
-        name = "DPLL"
-        plot(vars, time_dp, name, truths)
+        outfile_path = '../Output/2SAT_output.txt'
     elif mode == 2:
-        print(f'DumbSAT Total Timae: {reg_time} \t DPLL Total Time: {dp_time}\n')
-        print(f'Conflicting Outputs: {issues}')
-        plot_both(vars, time_dp, time_reg)
+        outfile_path = '../Output/both_output.txt'
+
+    with open(outfile_path, 'w') as outfile:
+        for key, wff in wffs.items():
+            nvars = int(wff[1][2])
+            nclauses = int(wff[1][3])
+
+            data = wff[2:]
+            data = [row[:2] for row in data]
+            data = [list(map(int, row)) for row in data]
+
+
+            if mode  == 0:
+                output = test_wff(data, nvars, nclauses)
+                outfile.write(f"Is {key} satisfiable? {'Yes' if output[2] else 'No'}\n")
+                if output[2]:
+                    outfile.write(f'Solution: {output[1][1:-1]}\n')
+                else:
+                    outfile.write(f'Solution: None\n')
+                outfile.write(f'Time (microseconds): {output[3]}\n\n')
+
+                reg_time += output[3]
+                vars.append(nvars)
+                time_reg.append(output[3])
+                truths.append(output[2])
+
+            elif mode == 1:
+                output_DPLL = test_wff_DPLL(data, nvars, nclauses)
+                outfile.write(f"Is {key} satisfiable? {'Yes' if output_DPLL[2] else 'No'}\n")
+                if output_DPLL[2]:
+                    outfile.write(f'Solution: {output_DPLL[1]}\n')
+                else:
+                    outfile.write(f'Solution: None\n')
+                outfile.write(f'Time (microseconds): {output_DPLL[3]}\n\n')
+
+                dp_time += output_DPLL[3]
+                vars.append(nvars)
+                time_dp.append(output_DPLL[3])
+                truths.append(output_DPLL[2])
+
+            elif mode == 2:
+                issues = []
+                output = test_wff(data, nvars, nclauses)
+                output_DPLL = test_wff_DPLL(data, nvars, nclauses)
+                reg_time += output[3]
+                dp_time += output_DPLL[3]
+                if (output[2] != output_DPLL[2]):
+                    issues.append((output, output_DPLL))
+                outfile.write(f'{key} of {len(wffs)}:\n')
+                outfile.write(f'DumbSAT Output: {output[2]} \t DPLL Output: {output_DPLL[2]}\n')
+                outfile.write(f'DumbSAT Time (microseconds): {output[3]} \t DPLL Time (microseconds): {output_DPLL[3]}\n\n')
+
+                vars.append(nvars)
+                time_dp.append(output_DPLL[3])
+                time_reg.append(output[3])
+
+
+        if mode == 0:
+            outfile.write(f'\nDumbSAT Total Time (microseconds): {reg_time}')
+            name = "DumbSAT"
+            plot(vars, time_reg, name, truths)
+        elif mode == 1:
+            outfile.write(f'\nDPLL Total Time (microseconds): {dp_time}')
+            name = "DPLL"
+            plot(vars, time_dp, name, truths)
+        elif mode == 2:
+            outfile.write(f'\nDumbSAT Total Time (microseconds): {reg_time} \t DPLL Total Time (microseconds): {dp_time}\n')
+            outfile.write(f'Conflicting Outputs: {issues}')
+            plot_both(vars, time_dp, time_reg)
 
     return 0
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <csv_file>")
+    if len(sys.argv) < 3:
+        print("Usage: python script.py <csv_file> {0,1,2}")
     else:
         main()
